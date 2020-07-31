b2a65f79503940b6cd78104849661d50265abc11
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2020-07-31 16:51:23 -0500

    adding arrays of possible numbers, letter, and symbols

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var nums = [0,1,2,3,4,5,6,7,8,9]
+var lcs = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
+var ucs = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]
+var syms = ["!","@","#","$","%","^","&","(",")","*","'","+",",",".","-","/",":",";","<",">","=","?","[","]","\\","_","|","}","{","~"]
 
 var charCount = prompt("How many characters do you want your password to be?");
   if (typeof(charChount) != "number") {
@@ -40,15 +44,17 @@ var wantSpec = confirm("Do you want special characters?");
 
   }
   else {
-    
+
   }
 // Write password to the #password input
+function generatePassword(){
+
+}
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
