654749cb10343a46a1b9b3fc707620b19cd7bb09
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2020-07-31 13:31:35 -0500

    adds ifelse statements to variables

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var charCount = prompt("How many characters do you want your password to be?");
-var wantUpper = confirm("Do you want upper case characters?");
-var wantLower = confirm("Do you want lower case characters?");
-var wantNum = confirm("Do you want numbers?");
-var wantSpec = confirm("Do you want special characters?");
 
+var charCount = prompt("How many characters do you want your password to be?");
+  if (typeof(charChount) != "number") {
+    alert("Please input valid number");
+    var charCount = prompt("How many characters do you want your password to be?");
+  } 
+  else if (charCount === 0){
+    alert("Please input valid number");
+    var charCount = prompt("How many characters do you want your password to be?");
+  } 
+  else {
+    parseInt(charCount);
+  }
+
+var wantUpper = confirm("Do you want upper case characters?");
+  if (wantUpper === true){
+
+  }
+  else {
+
+  }
+var wantLower = confirm("Do you want lower case characters?");
+  if (wantLower === ture){
+
+  }
+  else {
+
+  }
+var wantNum = confirm("Do you want numbers?");
+  if (wantNum === true){
+
+  }
+  else {
+
+  }
+var wantSpec = confirm("Do you want special characters?");
+  if (wantSpec === true){
+
+  }
+  else {
+    
+  }
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -18,8 +54,4 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-if (isNAN(charCount.value)) {
-  alert("Please input valid number");
-} ifelse (charCount.value == 0){
-  alert("Please input valid number");
-}
+
