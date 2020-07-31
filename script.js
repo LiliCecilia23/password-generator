cdbb5f38ded88273a199b39a42f57ccdf910aabb
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2020-07-31 13:02:08 -0500

    adding variables and validation

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var charCount = prompt("How many characters do you want your password to be?");
+var wantUpper = confirm("Do you want upper case characters?");
+var wantLower = confirm("Do you want lower case characters?");
+var wantNum = confirm("Do you want numbers?");
+var wantSpec = confirm("Do you want special characters?");
 
 // Write password to the #password input
 function writePassword() {
@@ -12,3 +17,9 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+if (isNAN(charCount.value)) {
+  alert("Please input valid number");
+} ifelse (charCount.value == 0){
+  alert("Please input valid number");
+}
