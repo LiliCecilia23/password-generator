a76b5b5063e3c0152e469c7171d99a4f1ee032f2
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2020-08-03 17:58:24 -0500

    changed variables

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var nums = [0,1,2,3,4,5,6,7,8,9]
-var lcs = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
-var ucs = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]
-var syms = ["!","@","#","$","%","^","&","(",")","*","'","+",",",".","-","/",":",";","<",">","=","?","[","]","\\","_","|","}","{","~"]
+var nums = [0,1,2,3,4,5,6,7,8,9];
+var lcs = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
+var ucs = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z];
+var syms = ["!","@","#","$","%","^","&","(",")","*","'","+",",",".","-","/",":",";","<",">","=","?","[","]","\\","_","|","}","{","~","\""];
 
 var charCount = prompt("How many characters do you want your password to be?");
   if (typeof(charChount) != "number") {
@@ -26,7 +26,7 @@ var wantUpper = confirm("Do you want upper case characters?");
 
   }
 var wantLower = confirm("Do you want lower case characters?");
-  if (wantLower === ture){
+  if (wantLower === true){
 
   }
   else {
