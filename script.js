f9af259e631242a1c719fd7d26b54a73227bf049
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2020-08-06 16:08:06 -0500

    cleaning up else if statements

--- script.js
+++ script.js
@@ -1,53 +1,53 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+//arrays for possible characters
 var nums = ["0","1","2","3","4","5","6","7","8","9"];
 var lcs = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var ucs = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var syms = ["!","@","#","$","%","^","&","(",")","*","'","+",",",".","-","/",":",";","<",">","=","?","[","]","\\","_","|","}","{","~","\""];
+var syms = ["!","@","#","$","%","^","&","(",")","*","'","+",",",".","-","/",":",";","<",">","=","?","[","]","_","|","}","{","~"];
 var allchars = [];
+
+//defines length of password and verifies validity of user input
 var charCount = prompt("How many characters do you want your password to be?");
-  if (typeof(charChount) != "number") {
+  if (isNaN(charCount) || charCount == 0) {
     alert("Please input valid number");
     var charCount = prompt("How many characters do you want your password to be?");
   } 
-  else if (charCount === 0){
-    alert("Please input valid number");
-    var charCount = prompt("How many characters do you want your password to be?");
-  } 
-  else {
-    parseInt(charCount);
-  }
-
+ 
+//checks what kind of characters the user wants
 var wantUpper = confirm("Do you want upper case characters?");
-  if (wantUpper === true){
-    var allchars = ucs.concat;
-  }
-
 var wantLower = confirm("Do you want lower case characters?");
-  if (wantLower === true){
-    var allchars = lws.concat;
-  }
-  
 var wantNum = confirm("Do you want numbers?");
-  if (wantNum === true){
-    var allchars = nums.concat;
-  }
-  
 var wantSpec = confirm("Do you want special characters?");
-  if (wantSpec === true){
-    var allchars = syms.concat;
-  }
-
+ 
+//makes sure user has selected at least one character type
 if (wantUpper == false && wantLower == false && wantNum == false && wantSpec == false){
   alert("Must choose to include at least one character type.");
+  location.reload();
 }
+//puts together arrays of wanted characters into allchars array
+else if (wantUpper === true){
+  var allchars = allchars.concat(uws);
+}
+else if (wantLower === true){
+  var allchars = allchars.concat(lws);
+}
+else if (wantNum === true){
+  var allchars = allchars.concat(nums);
+}
+else if (wantSpec === true){
+  var allchars = allchars.concat(syms);
+}
+
+
   
 // Write password to the #password input
-function generatePassword(l, allchars){
-  var pword = "";
+function generatePassword(charCount, allchars){
+  var password = "";
 
-  for (var i = 0; i === charCount; i++){
-    pword += allchars.charAt(Math.floor(Math.random) * allchars.length)
+  for (var i = 0; i == charCount.value - 1; i++){
+    password == allchars.charAt(Math.floor(Math.random) * allchars.length)
   }
 }
 function writePassword() {
