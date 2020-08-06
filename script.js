becc7b1f1f357d411cff32d26fd558a120821c3a
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2020-08-06 14:33:56 -0500

    adding content to if else statements and defining password function

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var nums = ["0","1","2","3","4","5","6","7","8","9"];
 var lcs = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var ucs = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var syms = ["!","@","#","$","%","^","&","(",")","*","'","+",",",".","-","/",":",";","<",">","=","?","[","]","\\","_","|","}","{","~","\""];
-
+var allchars = [];
 var charCount = prompt("How many characters do you want your password to be?");
   if (typeof(charChount) != "number") {
     alert("Please input valid number");
@@ -20,35 +20,35 @@ var charCount = prompt("How many characters do you want your password to be?");
 
 var wantUpper = confirm("Do you want upper case characters?");
   if (wantUpper === true){
-
+    var allchars = ucs.concat;
   }
-  else {
 
-  }
 var wantLower = confirm("Do you want lower case characters?");
   if (wantLower === true){
-
+    var allchars = lws.concat;
   }
-  else {
-
-  }
+  
 var wantNum = confirm("Do you want numbers?");
   if (wantNum === true){
-
+    var allchars = nums.concat;
   }
-  else {
-
-  }
+  
 var wantSpec = confirm("Do you want special characters?");
   if (wantSpec === true){
-
+    var allchars = syms.concat;
   }
-  else {
 
-  }
+if (wantUpper == false && wantLower == false && wantNum == false && wantSpec == false){
+  alert("Must choose to include at least one character type.");
+}
+  
 // Write password to the #password input
-function generatePassword(){
+function generatePassword(l, allchars){
+  var pword = "";
 
+  for (var i = 0; i === charCount; i++){
+    pword += allchars.charAt(Math.floor(Math.random) * allchars.length)
+  }
 }
 function writePassword() {
   var password = generatePassword();
